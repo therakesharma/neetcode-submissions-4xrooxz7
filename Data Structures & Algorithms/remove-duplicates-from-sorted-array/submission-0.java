1854c3b75b7c54fddf841a73edfa6e10b065a544
rahulrakesh25 <42341286+rahulrakesh25@users.noreply.github.com>
2026-04-11 00:06:23 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int i = 0;
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+                nums[i] = num;
+                i++;
+            }
+        }
+        return i;
+    }
+}
