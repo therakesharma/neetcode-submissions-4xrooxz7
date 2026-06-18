d3cdfc4731b85b3d5cc03373f3dee2e8d983309a
Rakesh kumar <42341286+therakesharma@users.noreply.github.com>
2026-06-19 00:30:17 +0530

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] getOrder(int[][] tasks) {
+        Map<int [], Integer> indexTaskMap = new HashMap<>();
+        Queue<int []> taskQueue = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        
+        for (int i = 0; i < tasks.length; i++) {
+            int [] task = tasks[i];
+            indexTaskMap.put(task, i);
+            taskQueue.offer(task);
+        }
+
+        int [] res = new int[tasks.length];
+        int i = 0;
+        long time = 0;
+
+        Queue<int []> processingQueue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        while (!taskQueue.isEmpty() || !processingQueue.isEmpty()) {
+            time = Math.max(time, taskQueue.peek()[0]);
+            System.out.println("time -> " + time);
+            while (!taskQueue.isEmpty() && time >= taskQueue.peek()[0]) {
+                processingQueue.offer(taskQueue.poll());
+            }
+
+            while (!processingQueue.isEmpty()) {
+                int [] task = processingQueue.poll();
+                res[i] = indexTaskMap.get(task);
+                time += task[1];
+                i++;
+                while (!taskQueue.isEmpty() && time >= taskQueue.peek()[0]) {
+                    processingQueue.offer(taskQueue.poll());
+                }
+            }
+        }
+
+        return res;
+        
+    }
+}
